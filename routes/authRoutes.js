c438c8ffb07dc5a0f65a9df0825c60f2e45a78a1
melissatedesco <tedesco.melissa.its2@gmail.com>
2026-09-25 14:56:18 +0200

    agg

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,41 @@ import express from 'express'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import Admin from '../models/Admin.js'
+import { authenticateAdmin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
+// rotta per registrare nuovo Admin (solo admin autenticati)
+router.post('/register', authenticateAdmin, async (req, res) => {
+    try {
+        const {username, password} = req.body
+
+        if (!username || !password) {
+            return res.status(400).json({ error: 'Username e password sono obbligatori'})
+        }
+
+        const existingAdmin =await Admin.findOne({ where: {username}})
+        if (existingAdmin) {
+            return res.status(409).json({ error: 'Username già esistente'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newAdmin = await Admin.create({
+        username,
+        password: hashedPassword
+    });
+
+        return res.status(201).json({
+        message: 'Admin creato con successo.',
+        admin: { id: newAdmin.id, username: newAdmin.username }
+    });
+  } catch (error) {
+    console.error('Errore durante la registrazione:', error);
+    return res.status(500).json({ error: 'Errore interno del server.' });
+  }
+});
+
+// login admin
 router.post('/login', async (req, res) => {
     try {
         const { username, password } = req.body ?? {}
